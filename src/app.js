ab82f294af9de8ee2d96946c5d7422ee589b071d
Srivastav <Utkarsh.Srivastav@motherson.com>
2026-06-19 14:43:01 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -3,22 +3,27 @@ const express = require('express');
 const app = express();// instance of an express.js application 
 
 // creating a new web server 
-//i have to call listen 
-
-app.use( "/",(req,res) =>{
-     //request handler
-  res.send("Hello from the server Dashboard");
-});
-app.use( "/home",(req,res) =>{
-     //request handler
-  res.send("Hello from the server home");
-});
+//i have to call listen
 
 
-app.use("/test", (req,res) =>{
-     //request handler
-  res.send("Hello from the server test");
-});
+//this will match all the HTTP method API calls to /test
+// app.use( "/user",(req,res) =>{
+//      //request handler
+//   res.send("Hello from the server user");
+// });
+
+//order matters in routing 
+// This will only handle get call to /user 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Utkarsh",
+lastName :"Srivastav"
+ } )
+})
+
+app.post("/user",(req,res)=>{
+  //saving data to DB.
+    res.send("Data Successfully saved to the db");
+})
 
 
 app.listen(3000, ()=>{
